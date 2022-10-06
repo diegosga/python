2f4e87c0ec1f42ae4b148c6f7b54f92689485830
diegosga <113614608+diegosga@users.noreply.github.com>
2022-10-05 22:19:44 -0300

    Delete bot.py

--- bot.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-pyautogui.PAUSE=1
-pyautogui.press("win")
-pyautogui.write("bloco")
-pyautogui.press("enter")
-pyautogui.write("seu pc foi invadido, deposite 50.000 bitcoins na conta x")
