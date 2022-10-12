75cbfe39d8248e517ee92dcf52b1dac2763f0397
diegosga <113614608+diegosga@users.noreply.github.com>
2022-10-12 01:08:32 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -2,12 +2,14 @@ from PySimpleGUI import PySimpleGUI as sg
 
 sg.theme('Reddit')
 layout = [
-[sg.Text('Nome do titular'), sg.Input(key='Nome do titular')],
-[sg.Text('Numero do cartão de credito'), sg.Input(key='Numero do cartão de credito')],
-[sg.Text('CVV'), sg.Input(key='CVV')],
+[sg.Text('Nome'), sg.Input(key='Nome')],
+[sg.Text('Numero de Telefone'), sg.Input(key='Numero de Telefone')],
+[sg.Text('Email'), sg.Input(key='Email')],
+[sg.Text('Senha'), sg.Input(key='Senha')],
+[sg.Text('Digite a senha novamente'), sg.Input(key='Digite a senha novamente')],
 [sg.Button('Confirmar')]
 ]
-janela=sg.Window('Cadastro de cartao', layout)
+janela=sg.Window('Cadastro', layout)
 while True:
     eventos, valores = janela.read()
     if eventos == sg.WINDOW_CLOSED:
