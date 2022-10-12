9f60e81a6226507d959db01aac7210f5cee78e79
diegosga <113614608+diegosga@users.noreply.github.com>
2022-10-12 00:52:40 -0300

    Delete botselenium.py

--- botselenium.py
+++
@@ -1,8 +0,0 @@
-from multiprocessing.connection import wait
-from playwright.sync_api import sync_playwright
-with sync_playwright() as p:
- browser = p.chromium.launch(headless=False)
- page = browser.new_page()
- page.goto("https://www.youtube.com/watch?v=NSxVV9hwJKo")
- page.click("span[id='video-title']")
- page.wait_for_timeout(10000000)
